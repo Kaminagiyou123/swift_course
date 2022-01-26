3a8c3d86051d2fd279cb5fb5fbf7c1d5373e0148
Kaminagiyou123 <78280747+Kaminagiyou123@users.noreply.github.com>
2022-01-25 19:21:34 -0800

    new Dicee complete

---
+++ Dicee-Swift/Dicee-Swift/Dicee_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftApp.swift
+//  Dicee-Swift
+//
+//  Created by ran you on 1/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
