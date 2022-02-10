44b47ff2d0d7be5172a5ef15b6941b01b8287d13
Kaminagiyou123 <78280747+Kaminagiyou123@users.noreply.github.com>
2022-02-10 14:23:22 -0800

    calc 2

---
+++ Calculator-Advanced-Swift-iOS13_2/Calculator/Model/CalculatorLogic.swift
@@ -0,0 +1,62 @@
+//
+//  CalculatorLogic.swift
+//  Calculator
+//
+//  Created by ran you on 2/10/22.
+//  Copyright © 2022 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorLogic {
+    private var number: Double?
+    
+    private var intermediateCalculation : (n1 :Double, calcMethod: String)?
+      
+    
+    mutating func setNumber(_ number:Double) {
+        self.number = number
+    }
+  
+    mutating func calculate (symbol:String)-> Double?{
+        
+    if let n =  number {
+        if symbol == "+/-" {
+                return n * -1
+            }
+            else if symbol == "AC" {
+                return 0
+            }
+            else if symbol == "%" {
+                return n * 0.01
+            }
+        else if symbol == "="{
+                        return  performTwoNumberCalculation(n2:n)
+                
+            } else {
+                intermediateCalculation = (n1: n, calcMethod: symbol)
+            }
+        
+       }
+        return nil
+    }
+
+   private func performTwoNumberCalculation(n2 :Double) -> Double?{
+       if let n1 = intermediateCalculation?.n1, let operation = intermediateCalculation?.calcMethod {
+         
+           if operation == "+" {
+               return n1 + n2
+           }
+           else if operation == "-" {
+               return n1 + n2
+           }
+           else if operation == "×" {
+               return n1 * n2
+           }
+           else if operation == "÷" {
+               return n1 / n2
+           }
+    }
+       return nil
+}
+}
